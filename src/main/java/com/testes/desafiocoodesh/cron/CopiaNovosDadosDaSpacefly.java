5a7820bd2a6fde5489cf2216ba92e4c246dd07eb
GustavoJPereira <gustavojosepereira2021@gmail.com>
2022-03-23 18:17:16 -0300

    feat: cron para ser executado todo dia as 9:00 para verificar os novos dados

---
+++ src/main/java/com/testes/desafiocoodesh/cron/CopiaNovosDadosDaSpacefly.java
@@ -0,0 +1,24 @@
+package com.testes.desafiocoodesh.cron;
+
+import com.testes.desafiocoodesh.repository.RepositoryUltimoIdSpacefly;
+import com.testes.desafiocoodesh.service.CopiarSpacefly;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CopiaNovosDadosDaSpacefly {
+
+    CopiarSpacefly copiarSpacefly = new CopiarSpacefly();
+
+    @Autowired
+    RepositoryUltimoIdSpacefly repositoryUltimoIdSpacefly;
+
+    //Cron para copiar novos conteúdos da SpaceFly API todo dia as 9 horas da manhã
+    @Scheduled(cron = "0 0 9 * * ?") // * * 9 ? * * *
+    public void CopiarDados() {
+        if (!repositoryUltimoIdSpacefly.findById(1L).isPresent()){
+            repositoryUltimoIdSpacefly.iniciarBanco();}
+        copiarSpacefly.copiarSpacefly();
+    }
+}
